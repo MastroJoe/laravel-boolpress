23b896aa88ebbaf9800129d6808209361af86f51
Giorgio <giorgio.pignattari@gmail.it>
2021-05-27 00:14:57 +0200

    ricerca tramite slug in show.blade

--- laravel-boolpress/app/Http/Controllers/PostController.php
+++ laravel-boolpress/app/Http/Controllers/PostController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
   public function show(string $slug) {
-    $slug = Post::where('slug', '=', $slug)->first();
+    $post = Post::where('slug', '=', $slug)->first();
 
     return view('guests.posts.show', compact('post'));
   }
