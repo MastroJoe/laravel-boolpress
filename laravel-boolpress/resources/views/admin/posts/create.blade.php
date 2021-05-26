58797124281c7f5089865f3ddec566dc1e5b1715
Giorgio <giorgio.pignattari@gmail.it>
2021-05-26 18:09:31 +0200

    creato form del create e creata rotta nel controller

--- laravel-boolpress/resources/views/admin/posts/create.blade.php
+++ laravel-boolpress/resources/views/admin/posts/create.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h3>Nuovo Post</h3>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <form action="{{ route('admin.posts.store') }}" method="post">
+        @csrf
+        @method('POST')
+        <div class="form-group">
+          <label for="title">Title</label>
+          <input class="form-control" id="title" type="text" name="title" value="">
+        </div>
+        <div class="form-group">
+          <label for="content">Content</label>
+          <input class="form-control" id="content" type="text" name="content" value="">
+        </div>
+        <button class="btn btn-primary" type="submit" name="button">Salva</button>
+      </form>
+    </div>
+  </div>
+</div>
 @endsection
