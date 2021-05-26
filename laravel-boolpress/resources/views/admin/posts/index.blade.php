b007ed647301f28aad257fc61a176f775829841f
Giorgio <giorgio.pignattari@gmail.it>
2021-05-26 17:59:33 +0200

    creazione vista create, form

--- laravel-boolpress/resources/views/admin/posts/index.blade.php
+++ laravel-boolpress/resources/views/admin/posts/index.blade.php
@@ -2,20 +2,24 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-      @foreach ($posts as $post)
-        <div class="col-md-3">
-            <div class="card">
-                <div class="card-header">
-                  {{ $post->title }}
-                </div>
-                <div class="card-body">
-                  {{ $post->content }}
-                </div>
-            </div>
-        </div>
-      @endforeach
+  <div class="row">
+    <div class="col-md-12">
+      <a href="{{ route('admin.posts.create')}}">Crea post</a>
     </div>
+  </div>
+  <div class="row justify-content-center">
+    @foreach ($posts as $post)
+      <div class="col-md-3">
+          <div class="card">
+              <div class="card-header">
+                {{ $post->title }}
+              </div>
+              <div class="card-body">
+                {{ $post->content }}
+              </div>
+          </div>
+      </div>
+    @endforeach
+  </div>
 </div>
-
 @endsection
