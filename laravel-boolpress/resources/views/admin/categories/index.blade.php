b7db7a81553eb83bbdb966ba7d87e0b1c0297946
Giorgio <giorgio.pignattari@gmail.it>
2021-05-28 18:27:37 +0200

    creazione vista show  categories

--- laravel-boolpress/resources/views/admin/categories/index.blade.php
+++ laravel-boolpress/resources/views/admin/categories/index.blade.php
@@ -2,39 +2,40 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <a class="btn btn-primary" href="{{route('admin.categories.create')}}">Nuovo categoria</a>
-      </div>
+  <div class="row">
+    <div class="col-md-12">
+      <a class="btn btn-primary" href="{{route('admin.categories.create')}}">
+        Nuova categoria
+      </a>
     </div>
-    <div class="row justify-content-center">
-      @foreach ($categories as $category)
-        <div class="col-md-3">
-            <div class="card">
-                <div class="card-header">{{$category->name}}</div>
+  </div>
+  <div class="row justify-content-center">
+    @foreach ($categories as $category)
+      <div class="col-md-3">
+          <div class="card">
+              <div class="card-header">{{ $category->name }}</div>
 
-                <div class="card-body">
-                  <div class="">
-                    <a class="btn btn-info" href="{{route('admin.categories.show', ['category' => $category->id])}}">
-                      Mostra
-                    </a>
-                    <a class="btn btn-primary" href="{{route('admin.categories.edit', ['category' => $category->id])}}">
-                      Modifica
-                    </a>
-                    <a class="btn btn-danger" onclick="event.preventDefault();
-                                  this.nextElementSibling.submit();">
-                      Elimina
-                    </a>
-                    <form action="{{route('admin.categories.destroy', ['category' => $category->id])}}" method="POST" style="display: none;">
-                        @csrf
-                        @method('DELETE')
-                    </form>
-                  </div>
+              <div class="card-body">
+                <div class="">
+                  <a class="btn btn-info" href="{{route('admin.categories.show', ['category' => $category->id])}}">
+                    Mostra
+                  </a>
+                  <a class="btn btn-primary" href="{{route('admin.categories.edit', ['category' => $category->id])}}">
+                    Modifica
+                  </a>
+                  <a class="btn btn-danger" onclick="event.preventDefault();
+                                this.nextElementSibling.submit();">
+                    Elimina
+                  </a>
+                  <form action="{{route('admin.categories.destroy', ['category' => $category->id])}}" method="POST" style="display: none;">
+                      @csrf
+                      @method('DELETE')
+                  </form>
                 </div>
-            </div>
-        </div>
-      @endforeach
-    </div>
+              </div>
+          </div>
+      </div>
+    @endforeach
+  </div>
 </div>
-
 @endsection
