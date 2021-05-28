2c8a3306b883fdc1d90db49042d88b9c46a5398d
Giorgio <giorgio.pignattari@gmail.it>
2021-05-28 18:47:34 +0200

    creata edit e aggiunti fillable a App\Category

--- laravel-boolpress/app/Category.php
+++ laravel-boolpress/app/Category.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+  protected $fillable = [
+    'name',
+    'slug',
+  ];
+
   public function posts()
   {
     return $this->hasMany('App\Post');
