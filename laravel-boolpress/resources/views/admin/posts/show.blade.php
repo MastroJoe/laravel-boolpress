1c80615fc077aaf331b42d9ddb445303dafdb31b
Giorgio <giorgio.pignattari@gmail.it>
2021-05-29 16:35:53 +0200

    modificata vista show di posts

--- laravel-boolpress/resources/views/admin/posts/show.blade.php
+++ laravel-boolpress/resources/views/admin/posts/show.blade.php
@@ -4,7 +4,15 @@
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-md-8">
-      <h1>{{ $post->title }}</h1>
+      <h1>Titolo: {{ $post->title }}</h1>
+      <h4>Categoria:
+        @if ($post->category)
+          {{ $post->category->name }}
+        @endif
+      </h4>
+
+      <img src="{{ asset('storage/'.$post->cover) }}" alt="{{ $post->title }}">
+
       <p>{{ $post->content }}</p>
     </div>
   </div>
