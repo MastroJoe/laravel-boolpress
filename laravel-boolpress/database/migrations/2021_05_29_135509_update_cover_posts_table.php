1707f1b1e0e9274210100014ae9e868d192446d6
Giorgio <giorgio.pignattari@gmail.it>
2021-05-29 16:03:54 +0200

    creazione e lancio della migration Cover

--- laravel-boolpress/database/migrations/2021_05_29_135509_update_cover_posts_table.php
+++ laravel-boolpress/database/migrations/2021_05_29_135509_update_cover_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdatePostsTable extends Migration
+class UpdateCoverPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,7 @@ class UpdatePostsTable extends Migration
     public function up()
     {
       Schema::table('posts', function (Blueprint $table) {
-        $table->unsignedBigInteger('category_id')->after('slug')->nullable();
-
-        $table->foreign('category_id')
-              ->references('id')
-              ->on('categories');
+        $table->text('cover')->after('slug')->nullable();
       });
     }
 
@@ -30,8 +26,7 @@ class UpdatePostsTable extends Migration
     public function down()
     {
       Schema::table('posts', function (Blueprint $table) {
-        $table->dropForeign('posts_category_id_foreign');
-        $table->dropColumn('category_id');
+        $table->dropColumn('cover');
       });
     }
 }
