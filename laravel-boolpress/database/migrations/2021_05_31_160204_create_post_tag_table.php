be05902b0787962c51951a5492b2a9cad4b69df3
Giorgio <giorgio.pignattari@gmail.it>
2021-05-31 18:17:15 +0200

    creata tabella con migrate

--- laravel-boolpress/database/migrations/2021_05_31_160204_create_post_tag_table.php
+++ laravel-boolpress/database/migrations/2021_05_31_160204_create_post_tag_table.php
@@ -14,8 +14,17 @@ class CreatePostTagTable extends Migration
     public function up()
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+          $table->unsignedBigInteger('post_id');
+
+          $table->foreign('post_id')
+                ->references('id')
+                ->on('posts');
+
+          $table->unsignedBigInteger('tag_id');
+
+          $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags');
         });
     }
 
