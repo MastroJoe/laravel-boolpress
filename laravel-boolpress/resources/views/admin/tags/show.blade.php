7d15a30a439882668346dad5790a21d788cfc6b7
Giorgio <giorgio.pignattari@gmail.it>
2021-05-31 17:15:17 +0200

    creazione viste di tags e sistemata la tabella migration

---
+++ laravel-boolpress/resources/views/admin/tags/show.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <h1>{{ $category->name }}</h1>
+    </div>
+  </div>
+</div>
+@endsection
--- laravel-boolpress/routes/web.php
+++ laravel-boolpress/routes/web.php
@@ -41,6 +41,7 @@ use Illuminate\Support\Facades\Route;
      Route::get('/', 'HomeController@index')->name('index');
      Route::resource('posts', 'PostController');
      Route::resource('categories', 'CategoryController');
+     Route::resource('tags', 'TagController');
    });
   /**
   *Rotte Admin
